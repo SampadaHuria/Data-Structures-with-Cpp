bed297968bc78e8dd84aaf3bb178d5c9c52ccf4f
Sampada Huria <95231524+SampadaHuria@users.noreply.github.com>
2022-09-03 21:10:53 +0530

    Update Say digits.cpp

--- Say digits.cpp
+++ Say digits.cpp
@@ -1,27 +1,36 @@
-#include<iostream>
+#include<iostream> 
 using namespace std;
 
-void sayDigit(int n , int arr[])
-{
+void sayDigit(int n, string arr[]) {
 
     //base case
-    if(n==0)
-        return;
+    if(n == 0)
+        return ;
 
-    //processing
-    int digit=n%10;
-    n=n/10;
+    //processing 
+    int digit = n % 10;
+    n = n / 10;
+    
 
     //recursive call
-    sayDigit(n,arr);
-    cout<<arr[digit]<<" ";
+    sayDigit(n, arr);
+
+    cout << arr[digit] << " ";
+
 }
 
-int main()
-{
-    string arr[10]={"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+int main() {
+
+    string arr[10] = {"zero", "one", "two", "three",
+                         "four", "five", "six"
+                        , "seven", "eight", "nine"};
     int n;
-    cin>>n;
-    sayDigit(n,arr);
-    cout<<endl;
+    cin >> n;
+
+    cout << endl << endl << endl ;
+    sayDigit(n, arr);
+    cout << endl << endl << endl ;
+
+
+    return 0;
 }
