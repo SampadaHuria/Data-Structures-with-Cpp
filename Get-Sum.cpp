39365648104ae2e8d79874550eaaa3586b9aa347
Sampada Huria <95231524+SampadaHuria@users.noreply.github.com>
2022-09-06 10:29:45 +0530

    Create Get-Sum.cpp

---
+++ Get-Sum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+ int getSum(int arr[],int size)
+ {
+
+     //base case
+     if(size==0)
+     {
+         return 0;
+     }
+     if(size==1)
+     {
+
+         return arr[0];
+     }
+     int remainingPart=getSum(arr+1,size-1);
+     int sum=arr[0]+remainingPart;
+     return sum;
+ }
+ int main()
+ {
+
+     int arr[5]={3,2,1,5,6};
+     int size=5;
+     int sum=getSum(arr,size);
+     cout<<"Sum is "<<sum<<endl;
+     return 0;
+ }
+
