f6c93c778136b12a400fd88767fc717252854e88
Sampada Huria <95231524+SampadaHuria@users.noreply.github.com>
2022-09-07 08:55:39 +0530

    Create Bubble Sort.cpp

---
+++ Sorting/Bubble Sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void sortArray(int arr[],int n)
+{
+    //base case-already sorted
+    if(n==0||n==1)
+    {
+        return ;
+    }
+    //1 case solved - LARGEST ELEMENT KO END MEIN RAKH DEGA
+    for(int i=0;i<n-1;i++)
+    {
+
+        if(arr[i]>arr[i+1])
+        {
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    sortArray(arr,n-1);
+}
+int main()
+{
+    int arr[5]={2,5,1,6,9};
+    sortArray(arr,5);
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<"";
+    }
+}
