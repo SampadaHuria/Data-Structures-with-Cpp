6b7f0865535f847425422e21e35baf15502f6bee
Sampada Huria <95231524+SampadaHuria@users.noreply.github.com>
2022-09-26 19:01:11 +0530

    Create Reverse Stack.cpp

---
+++ Reverse Stack.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+
+void insertAtBottom(stack<int>&s,int element)
+{
+    //base case
+    if(s.empty())
+    {
+        s.push(element);
+        return;
+    }
+
+    int num=s.top();
+    s.pop();
+
+    //recursive call
+    insertAtBottom(s,element);
+    s.push(num);
+}
+void reverseStack(stack<int>&stack)
+{
+    //base case
+    if(stack.empty())
+    {
+        return;
+    }
+    int num=stack.top();
+    stack.pop();
+
+    //recursive call
+    reverseStack(stack);
+    insertAtBottom(stack,num);
+}
